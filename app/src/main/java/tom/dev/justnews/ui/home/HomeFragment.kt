fa233d8988197e27998024dfdc976a10185ceff1
wooyoung <djdj7087@gmail.com>
2022-07-31 20:28:13 +0900

    resolves #5 navigation configuration

--- app/src/main/java/tom/dev/justnews/ui/home/HomeFragment.kt
+++ app/src/main/java/tom/dev/justnews/ui/home/HomeFragment.kt
@@ -1,8 +1,26 @@
 package tom.dev.justnews.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import tom.dev.justnews.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding? get() = _binding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
